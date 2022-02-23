9f7e675507bb7039021d0998f06a62513e424ca3
Leonardo Pinto Silva Calazans <leonardopintosilva@hotmail.com>
2022-02-23 20:48:23 -0300

    chore/new-title-login

--- src/screens/Signin/index.tsx
+++ src/screens/Signin/index.tsx
@@ -21,9 +21,9 @@ export function Signin(){
             />
             <View style={styles.content}>
                 <Text style={styles.title}>
-                    Organize {`\n`}
-                    suas jogatinas {`\n`}
-                    facilmente
+                    Conecte-se {`\n`}
+                    e organize suas {`\n`} 
+                    jogatinas 
                 </Text>
                 <Text style={styles.subtitle}>
                     Crie grupos para jogar seus games {`\n`}
