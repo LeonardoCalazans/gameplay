9d0bc582a27739547c887eb0ae128d8e0a34a8fc
Leonardo Pinto Silva Calazans <leonardopintosilva@hotmail.com>
2022-06-29 21:40:14 -0300

    build: config libs, babel, packege

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,27 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ["inline-dotenv"]
+    plugins: [
+      "inline-dotenv",
+      [
+        'module-resolver',
+        {
+          extensions: [
+            '.js',
+            '.jsx',
+            '.ts',
+            '.tsx',
+            '.android.js',
+            '.android.tsx',
+            '.ios.js',
+            '.ios.tsx',
+          ],
+          root: ['./src'],
+          alias: {
+            "~components": "./src/components",
+          },
+        },
+      ]
+    ]
   };
 };
