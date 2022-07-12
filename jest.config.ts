404e5b0e12a8269df45948743ccf46110da25243
Leonardo Pinto Silva Calazans <leonardopintosilva@hotmail.com>
2022-07-11 23:38:55 -0300

    fix-build: adding config jest expo

--- jest.config.ts
+++ jest.config.ts
@@ -5,6 +5,24 @@ import { defaults } from 'jest-config';
 const config: Config.InitialOptions = {
     moduleFileExtensions: [...defaults.moduleFileExtensions, 'mts'],
     verbose: true,
+    preset: "jest-expo",
+    transform: {
+        "\\.[jt]sx?$": "babel-jest",
+        '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+            '<rootDir>/fileTransformer.js',
+    },
+    transformIgnorePatterns: [
+        "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)"
+    ],
+    collectCoverage: true,
+    collectCoverageFrom: [
+        "**/*.{js,jsx}",
+        "!**/coverage/**",
+        "!**/node_modules/**",
+        "!**/babel.config.js",
+        "!**/jest.setup.js"
+    ],
+    setupFiles: ["./jestSetupFile.js"]
 };
 
 export default config;
