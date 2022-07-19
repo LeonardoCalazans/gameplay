ada332190c1a07f74caa60815bdad18506161846
Leonardo Pinto Silva Calazans <leonardopintosilva@hotmail.com>
2022-07-19 15:18:24 -0300

    feat: adding test in component Typography

--- src/components/Typography/styles.ts
+++ src/components/Typography/styles.ts
@@ -1,8 +1,7 @@
+import { moderateScale } from 'react-native-size-matters';
 import styled from 'styled-components/native';
-import { theme } from '../../global/styles/theme';
 
 export const Text = styled.Text<TypographyType>`
-  color: '#DDE3F0';
-  font-size: large;
-  line-height: 10px;
+  color: ${({ theme }) => theme.colors.heading};
+  font-size: ${moderateScale(13)}px;
 `;
