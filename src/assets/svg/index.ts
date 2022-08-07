9f5e6531e52f905c9b132c238f8c003ca369aa9b
Leonardo Pinto Silva Calazans <leonardopintosilva@hotmail.com>
2022-08-07 01:15:04 -0300

    feat: adding index file in mocks and svgs

---
+++ src/assets/svg/index.ts
@@ -0,0 +1,7 @@
+export { default as CalendarSvg } from './calendar.svg';
+export { default as DiscordSvg } from './discord.svg';
+export { default as DuelSvg } from './duel.svg';
+export { default as FunSvg } from './fun.svg';
+export { default as PlayerSvg } from './player.svg';
+export { default as RankedSvg } from './ranked.svg';
+export { default as trainingSvg } from './training.svg';
